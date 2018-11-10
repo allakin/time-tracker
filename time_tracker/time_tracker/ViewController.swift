9ebcead89841c9709382e027dc269c3969eaf1a5
“allakin” <19paul91@mail.ru>
2018-11-10 15:53:56 +0300

    default files

---
+++ time_tracker/time_tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  time_tracker
+//
+//  Created by Pavel on 10/11/2018.
+//  Copyright © 2018 Pavel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
